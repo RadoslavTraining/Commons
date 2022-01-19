0e99e9905c8f0a5b9bae0037182712dab4f0de0b
RadoslavMonov <radoslav.monov@estafet.com>
2022-01-19 16:12:42 +0200

    main added ItemNotFoundException

---
+++ src/main/java/com/monov/commons/exceptions/ItemNotFoundException.java
@@ -0,0 +1,13 @@
+package com.monov.commons.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemNotFoundException extends RuntimeException{
+    private Long id;
+}
